7b5c36de3b090c3a9e03ecba0379762ec4ddf6b6
JosueBarbosaBarrera <josu_ex@hotmail.com>
2021-03-27 01:35:29 -0600

    toggle

--- seleniumToggle.py
+++ seleniumToggle.py
@@ -1,13 +1,25 @@
 import unittest
-from selenium import webdriver
+from selenium import webdriver  
 from selenium.webdriver.common.keys import Keys
 import time
 
-class toggleTest(self):
+class toggleTest(unittest.TestCase):
 
     def setUp(self):
         self.drive = webdriver.Chrome(executable_path=r"C:\driverChrome\driverChrome.exe")
 
     def test_using_toggle(self):
         driver = self.driver
-        driver.get("")
+        driver.get("https://www.w3schools.com/howto/howto_css_switch.asp")
+        time.sleep(5)
+        toggle = driver.find_element_by_xpath('//*[@id="main"]/label[3]/div')
+        toggle.click()
+        time.sleep(5)
+        toggle.click()
+        time.sleep(5)
+
+    def tearDown(self):
+        self.driver.close()
+
+if __name__ == "__main__":
+    unittest.main()
